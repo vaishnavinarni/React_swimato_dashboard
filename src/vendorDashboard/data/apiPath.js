ae197e309ee2057cbe42a082a9832b0e0188cd24
unknown <vaishnavi.n5b5@gmail.com>
2024-08-11 11:43:35 +0530

    changed path to live in api_url

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,2 +1,2 @@
-export const API_URL="http://localhost:4000";
-//export const API_URL="http://backend-nodejs-swimato.onrender.com"
+//export const API_URL="http://localhost:4000";
+export const API_URL="http://backend-nodejs-swimato.onrender.com"
